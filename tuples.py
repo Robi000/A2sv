ec3ae597a8f0edd521618eaa1fa60815c2ae917c
robi000 <smwefyelem@gmail.com>
2023-02-10 02:03:33 +0300

    this is edited

--- tuples.py
+++ tuples.py
@@ -2,3 +2,5 @@ if __name__ == '__main__':
     n = int(input())
     integer_list = tuple(map(int, input().split()))
     print(hash(integer_list))
+    ponsibility to request a review for this task from
+
